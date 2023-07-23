cb067e4646b825fa38874300665db1c22bf8a80e
Parham-Mehrabi <parham.mehrabi.webdev@gmail.com>
2023-07-24 02:17:03 +0330

    create second page for login

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from account.views import AccountIndexView
+from account.views import AccountIndexView, AccountLoginView
 
+app_name = 'account'
 
 urlpatterns = [
     path('', AccountIndexView.as_view(), name='index'),
+
+    # auth
+    path('login/', AccountLoginView.as_view(), name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 
 class AccountIndexView(TemplateView):
     """
         renders the index.html for the root '/'
     """
-    template_name = 'account/index.html'
+    template_name = 'index.html'
+
+
+class AccountLoginView(LoginView):
+    template_name = 'account/login.html'
+    success_url = reverse_lazy('account:index')
+    next_page = reverse_lazy('account:index')
+
