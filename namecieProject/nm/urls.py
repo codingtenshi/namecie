b4f3f93291f538deabbd3acc35b197d5112181e3
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-24 16:18:05 +0200

    change button to link

--- namecieProject/nm/urls.py
+++ namecieProject/nm/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path, include
-from nm.views import strona_startowa
+from nm.views import strona_startowa, zaloguj
+from . import views
 
 urlpatterns = [
-    path('', strona_startowa),
+    path('', strona_startowa, name='strona_startowa'),
+    path('zaloguj/', views.zaloguj, name='zaloguj'),
 ]
--- namecieProject/nm/views.py
+++ namecieProject/nm/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def strona_startowa(request):
     return render(request, 'namecie.pl.html')
 
-
-
+def zaloguj(request):
+    return render(request, 'zaloguj.html')
 
