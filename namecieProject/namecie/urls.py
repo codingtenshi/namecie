e628a97efb3d4d8f6fa45aa0a40230a80fca7e1d
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-27 17:17:51 +0200

    modified requirements.txt and namecie/urls.py files

--- namecieProject/namecie/urls.py
+++ namecieProject/namecie/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nm.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
