e89e329802ece9ea27ba8992cc2bc0c4eb56b111
Tenshi Codes <karina.gornicka@gmail.com>
2023-08-02 13:58:43 +0200

    Added session to vievs.py file

--- namecieProject/nm/views.py
+++ namecieProject/nm/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
 
 def index(request):
     return render(request, 'index.html')
 
 def my_profile(request):
+    sdk_url = settings.ORY_SDK_URL
+    sess = requests.get(
+            f"{sdk_url}/sessions/whoami",
+            cookies=request.COOKIES
+        )
+    traits = sess.json().get('identity', {}).get('traits', None)
+    
+        
     context = {
         'user': request.user,
+        'first_name' : traits.get('first_name', None),
+        'last_name' : traits.get('last_name', None),
+        'email' : traits.get('email', None),
+        'picture' : traits.get('picture', None),
+       
     }
     return render(request, 'my_profile.html', context)
 
