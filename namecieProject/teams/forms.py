17c8920b7260e84461396967f37b6087f1d948a7
Tenshi Codes <karina.gornicka@gmail.com>
2023-08-13 10:36:09 +0200

    Update forms.py

--- namecieProject/teams/forms.py
+++ namecieProject/teams/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator,RegexValidator
 
 class TeamForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    team_description = forms.CharField(widget=forms.Textarea)
-    year_founded = forms.IntegerField()
+    name = forms.CharField(max_length=100, label='Nazwa')
+    team_description = forms.CharField(widget=forms.Textarea, label='Opis grupy')
+    year_founded = forms.IntegerField(label="Rok założenia")
+   
